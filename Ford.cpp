6a560582304259bd6abb8a898d3bbf157e007fc7
Adam Long <macadam.long@gmail.com>
2016-04-07 14:21:32 +0000

    builds now

--- Ford.cpp
+++ Ford.cpp
@@ -2,6 +2,7 @@
 #include "OI.h"
 #include "RobotMap.h"
 #include "Commands/Autonomous/Autonomous.h"
+CommandGroup* Ford::aut = NULL;
 OI* Ford::oi = NULL;
 Ford::Ford() {
   oi = new OI();
@@ -13,7 +14,7 @@ void Ford::DisabledPeriodic() {
   Scheduler::GetInstance()->Run();
 }
 void Ford::AutonomousInit() {
-  aut = new Autonomous(-1);
+  aut = new Autonomous(SmartDashboard::GetNumber("Auto Sequence"));
   printf("Enabling Auto Sequence %f\n", SmartDashboard::GetNumber("Auto Sequence"));
   if(aut != NULL) {
     aut->Start();
