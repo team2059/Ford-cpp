ba288b2d0185f913ff4052ff3c74103082a1cec4
Adam Long <macadam.long@gmail.com>
2016-04-13 18:00:54 +0000

    failed/incomplete attempt to switch use pid on the drive encoders

---
+++ Subsystems/DriveEncoderPID.h
@@ -0,0 +1,90 @@
+#ifndef DRIVEENCODERPID_H
+#define DRIVEENCODERPID_H
+
+#include "WPILib.h"
+#include "Commands/PIDSubsystem.h"
+/**
+ * @brief Collects data from the wheel encoders
+ *
+ * Collects data and processes data from IR wheel encoders
+ */
+class DriveEncoderPID: public PIDSubsystem {
+ private:
+  DigitalInput *rightWheelSensor, //<<! Right wheel sensor
+              *leftWheelSensor; //<<! Left wheel sensor
+  int rightWheelEncoderTicks, //<<! Right wheel encoder ticks
+      leftWheelEncoderTicks, //<<! Left wheel encoder ticks
+      rightWheelEncoderTarget, //<! Right wheel encoder target
+      leftWheelEncoderTarget; //<! Left wheel encoder target
+  bool rightWheelLastState,
+       leftWheelLastState;
+ public:
+  /**
+   * @brief Constructs DriveEncoderPID
+   */
+  DriveEncoderPID();
+  /**
+   * @brief Does nothing for now
+   */
+  void InitDefaultCommand();
+  /**
+   * @brief Returns the PID input
+   *
+   * @return the pid input
+   */
+  double ReturnPIDInput();
+  /**
+   * @brief Updates the right wheel encoder
+   */
+  void UpdateRightWheelEncoder();
+  /**
+   * @brief Updates the left wheel encoder
+   */
+  void UpdateLeftWheelEncoder();
+  /**
+   * @brief Sets right wheel encoder target
+   */
+  void SetRightWheelEncoderTarget(int target);
+  /**
+   * @brief Sets left wheel encoder target
+   */
+  void SetLeftWheelEncoderTarget(int target);
+  /**
+   * @brief Gets current right wheel encoder target
+   */
+  int GetRightWheelEncoderTarget();
+  /**
+   * @brief Gets current left wheel encoder target
+   */
+  int GetLeftWheelEncoderTarget();
+  /**
+   * @brief Gets current amount of right encoder ticks
+   */
+  int GetRightWheelEncoderTicks();
+  /**
+   * @brief Gets current amount of left encoder ticks
+   */
+  int GetLeftWheelEncoderTicks();
+  /**
+   * @brief Returns true if the encoder has hit the target
+   */
+  bool GetRightWheelEncoderHitTarget();
+  /**
+   * @brief Returns true if the encoder has hit the target
+   */
+  bool GetLeftWheelEncoderHitTarget();
+  /**
+   * @brief Resets the right encoder tick count (leaves target)
+   */
+  void ResetRightWheelTicks();
+  /**
+   * @brief Resets the left encoder tick count (leaves target)
+   */
+  void ResetLeftWheelTicks();
+  /**
+   * @brief Resets the tick count for both sides
+   */
+  void ResetWheelTicks();
+};
+#endif
+// vim: ts=2:sw=2:et
